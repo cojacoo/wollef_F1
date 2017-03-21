8f5253adb12f44cc68ec2c5b508a30d108c0890b
cojack <conrad.jackisch@kit.edu>
2017-03-21 08:12:11 +0100

    convert to gif script with path

--- convert_gif.sh
+++ convert_gif.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-cd /work/kit/iwg/oj4748/weierbach
-#convert eWlef_Dm2*.pdf Dm2.gif
-convert eWlef_Do2*.pdf Do2.gif
-convert eWlef_Dj2*.pdf Dj2.gif
-convert eWlef_Ej2*.pdf Ej2.gif
-convert eWlef_Em2*.pdf Em2.gif
-convert eWlef_Eo2*.pdf Eo2.gif
-convert eWlef_Fj2*.pdf Fj2.gif
-convert eWlef_Fm2*.pdf Fm2.gif
-convert eWlef_Fo2*.pdf Fo2.gif
+convert /work/kit/iwg/oj4748/weierbach/results/eWlef_Dm2*.pdf /work/kit/iwg/oj4748/weierbach/results/Dm2.gif
+convert /work/kit/iwg/oj4748/weierbach/results/eWlef_Do2*.pdf /work/kit/iwg/oj4748/weierbach/results/Do2.gif
+convert /work/kit/iwg/oj4748/weierbach/results/eWlef_Dj2*.pdf /work/kit/iwg/oj4748/weierbach/results/Dj2.gif
+convert /work/kit/iwg/oj4748/weierbach/results/eWlef_Ej2*.pdf /work/kit/iwg/oj4748/weierbach/results/Ej2.gif
+convert /work/kit/iwg/oj4748/weierbach/results/eWlef_Em2*.pdf /work/kit/iwg/oj4748/weierbach/results/Em2.gif
+convert /work/kit/iwg/oj4748/weierbach/results/eWlef_Eo2*.pdf /work/kit/iwg/oj4748/weierbach/results/Eo2.gif
+convert /work/kit/iwg/oj4748/weierbach/results/eWlef_Fj2*.pdf /work/kit/iwg/oj4748/weierbach/results/Fj2.gif
+convert /work/kit/iwg/oj4748/weierbach/results/eWlef_Fm2*.pdf /work/kit/iwg/oj4748/weierbach/results/Fm2.gif
+convert /work/kit/iwg/oj4748/weierbach/results/eWlef_Fo2*.pdf /work/kit/iwg/oj4748/weierbach/results/Fo2.gif
