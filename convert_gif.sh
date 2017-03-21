4448938e5b96f5f47d1a108f16f381dfa5668012
cojack <conrad.jackisch@kit.edu>
2017-03-21 08:10:15 +0100

    convert to gif script added

---
+++ convert_gif.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#convert eWlef_Dm2*.pdf Dm2.gif
+convert eWlef_Do2*.pdf Do2.gif
+convert eWlef_Dj2*.pdf Dj2.gif
+convert eWlef_Ej2*.pdf Ej2.gif
+convert eWlef_Em2*.pdf Em2.gif
+convert eWlef_Eo2*.pdf Eo2.gif
+convert eWlef_Fj2*.pdf Fj2.gif
+convert eWlef_Fm2*.pdf Fm2.gif
+convert eWlef_Fo2*.pdf Fo2.gif
