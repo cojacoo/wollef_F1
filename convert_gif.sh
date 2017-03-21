83b4355473babd1363782b121e195e56bc952b08
cojack <conrad.jackisch@kit.edu>
2017-03-21 08:22:18 +0100

    convert to gif script with correct path

--- convert_gif.sh
+++ convert_gif.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-convert /work/kit/iwg/oj4748/weierbach/results/eWlef_Dm2*.pdf /work/kit/iwg/oj4748/weierbach/results/Dm2.gif
-convert /work/kit/iwg/oj4748/weierbach/results/eWlef_Do2*.pdf /work/kit/iwg/oj4748/weierbach/results/Do2.gif
-convert /work/kit/iwg/oj4748/weierbach/results/eWlef_Dj2*.pdf /work/kit/iwg/oj4748/weierbach/results/Dj2.gif
-convert /work/kit/iwg/oj4748/weierbach/results/eWlef_Ej2*.pdf /work/kit/iwg/oj4748/weierbach/results/Ej2.gif
-convert /work/kit/iwg/oj4748/weierbach/results/eWlef_Em2*.pdf /work/kit/iwg/oj4748/weierbach/results/Em2.gif
-convert /work/kit/iwg/oj4748/weierbach/results/eWlef_Eo2*.pdf /work/kit/iwg/oj4748/weierbach/results/Eo2.gif
-convert /work/kit/iwg/oj4748/weierbach/results/eWlef_Fj2*.pdf /work/kit/iwg/oj4748/weierbach/results/Fj2.gif
-convert /work/kit/iwg/oj4748/weierbach/results/eWlef_Fm2*.pdf /work/kit/iwg/oj4748/weierbach/results/Fm2.gif
-convert /work/kit/iwg/oj4748/weierbach/results/eWlef_Fo2*.pdf /work/kit/iwg/oj4748/weierbach/results/Fo2.gif
+convert /work/kit/iwg/oj4748/wollef_F2/results/eWlef_Dm2*.pdf /work/kit/iwg/oj4748/wollef_F2/results/Dm2.gif
+convert /work/kit/iwg/oj4748/wollef_F2/results/eWlef_Do2*.pdf /work/kit/iwg/oj4748/wollef_F2/results/Do2.gif
+convert /work/kit/iwg/oj4748/wollef_F2/results/eWlef_Dj2*.pdf /work/kit/iwg/oj4748/wollef_F2/results/Dj2.gif
+convert /work/kit/iwg/oj4748/wollef_F2/results/eWlef_Ej2*.pdf /work/kit/iwg/oj4748/wollef_F2/results/Ej2.gif
+convert /work/kit/iwg/oj4748/wollef_F2/results/eWlef_Em2*.pdf /work/kit/iwg/oj4748/wollef_F2/results/Em2.gif
+convert /work/kit/iwg/oj4748/wollef_F2/results/eWlef_Eo2*.pdf /work/kit/iwg/oj4748/wollef_F2/results/Eo2.gif
+convert /work/kit/iwg/oj4748/wollef_F2/results/eWlef_Fj2*.pdf /work/kit/iwg/oj4748/wollef_F2/results/Fj2.gif
+convert /work/kit/iwg/oj4748/wollef_F2/results/eWlef_Fm2*.pdf /work/kit/iwg/oj4748/wollef_F2/results/Fm2.gif
+convert /work/kit/iwg/oj4748/wollef_F2/results/eWlef_Fo2*.pdf /work/kit/iwg/oj4748/wollef_F2/results/Fo2.gif
